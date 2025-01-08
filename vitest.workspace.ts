2ee4d0a93adc91272d694d6c18d8a6df793b92dc
lqf <maculosa@163.com>
2025-01-08 09:30:36 +0800

    feat: add vitest.workspace.ts

---
+++ vitest.workspace.ts
@@ -0,0 +1,9 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./packages/datav/vitest.config.ts",
+  "./packages/hooks/vitest.config.ts",
+  "./packages/draw/vitest.config.ts",
+  "./playground/vite.config.ts",
+  "./packages/procomponent/vitest.config.ts"
+])
