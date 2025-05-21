6851c14f73e8c7078d77f9fd64a49b39fd14e2e3
lqf <maculosa@163.com>
2025-05-21 17:58:05 +0800

    chore: 更新依赖版本以提升兼容性和性能
    更新了多个依赖包到最新版本，包括 @vitest、vite、vue 等，以提升项目的兼容性和性能。同时调整了 pnpm-workspace.yaml 中的包路径，并替换了 vite-plugin-vue-layouts 为 vite-plugin-vue-layouts-next

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -17,7 +17,7 @@ import uslug from 'uslug'
 import Icons from 'unplugin-icons/vite'
 import Unocss from 'unocss/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
-import Layouts from 'vite-plugin-vue-layouts'
+import Layouts from 'vite-plugin-vue-layouts-next'
 import { autoImportRaw } from './build/plugins/vite-plugin-auto-import-raw'
 
 // https://vite.dev/config/
