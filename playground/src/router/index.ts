d6ba5d126b8ee364d2dc77cf70f915409ce34917
lqf <maculosa@163.com>
2025-07-23 09:51:18 +0800

    refactor(playground): 移除无用代码并调整路由配置
    - 删除了路由配置中的 console.log 语句
    - 从 pnpm-workspace.yaml 中移除了 play 包

--- playground/src/router/index.ts
+++ playground/src/router/index.ts
@@ -3,8 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 
-console.log(routes)
-
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
