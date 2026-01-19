b4af1aae5e5a355adbcdbdf600b76d46d92e17cc
lqf <maculosa@163.com>
2026-01-19 12:00:13 +0800

    chore: 删除测试用的 test-cn.vue 文件

--- apps/playground/src/test-cn.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div :class="cn('p-4', 'bg-blue-500', { 'text-white': true }, 'rounded-lg')">
-    测试 cn 函数
-  </div>
-</template>
-
-<script setup lang="ts">
-// cn 函数会自动导入
-</script>
